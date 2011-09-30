5dcadd242be2712cdca729d6d7eea02210672fd6
Radamanthus Batnag <rad@batnag.org>
2011-09-30 18:19:04 +0800

    Removed corona-game-template as a submodule

---
+++ lib/nakor/generators/corona-game-template/loadmenu.lua
@@ -0,0 +1,33 @@
+module(..., package.seeall)
+
+require "init_buttons"
+
+-- Main function - MUST return a display.newGroup()
+function new()
+  local localGroup = display.newGroup()
+
+  local theTimer
+  local loadingImage
+
+  local showLoadingScreen = function()
+    loadingImage = display.newImageRect( "splash_screen.png", 480, 320 )
+    loadingImage.x = display.contentWidth
+    loadingImage.y = display.contentHeight
+    localGroup:insert(loadingImage)
+
+    local goToLevel = function()
+      director:changeScene( "menu" )
+    end
+
+    math.randomseed( os.time() )
+    theTimer = timer.performWithDelay( 1000, goToLevel, 1 )
+  end
+
+  showLoadingScreen()
+
+  unloadMe = function()
+  end
+
+  -- MUST return a display.newGroup()
+  return localGroup
+end
